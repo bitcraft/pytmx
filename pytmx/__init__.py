b91c00a5dc24c5f66786d35e0a84a5d1ca306597
bitcraft <leif.theden@gmail.com>
2014-03-25 17:11:58 -0500

    update version number

--- pytmx/__init__.py
+++ pytmx/__init__.py
@@ -2,5 +2,5 @@ from .tmxloader import load_pygame
 from .utils import build_rects
 from .pytmx import *
 
-__version__ = '3.17.0'
+__version__ = '3.17.1'
 __author__ = 'Leif Theden (leif.theden@gmail.com'
