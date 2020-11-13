4ed7a1afbd07eaec2fb6f6820b42d57d79a8b928
Leif Theden <leif.theden@gmail.com>
2020-11-13 13:20:01 -0600

    acid test

---
+++ apps/data/acid.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="acid.tsx" tilewidth="160" tileheight="160" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="160" height="160" source="acid6.png"/>
+ </tile>
+</tileset>
