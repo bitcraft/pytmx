070680418e832e1d85643e13fcd0f577d7ca6798
Leif Theden <leif.theden@gmail.com>
2021-04-20 08:06:16 -0500

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="PyTMX",
-    version='3.24',
+    version='3.25',
     description='loads tiled tmx maps.  for python 3.3+',
     author='bitcraft',
     author_email='leif.theden@gmail.com',
